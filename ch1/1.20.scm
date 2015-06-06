5b8bfb6e308af1a6abffa5125e4bebd0f358d643
Christopher Amatangelo <christopheramatangelo@74.114.238.24.in-addr.arpa.hsd.gullscove.ny.hwccustomers.com>
2015-06-06 08:01:43 -0400

    1.20

--- ch1/1.20.scm
+++ ch1/1.20.scm
@@ -6,3 +6,37 @@
 ; illustrate the process generated in evaluating (gcd 206 40) and indicate the remainder 
 ; operations that are actually performed. How many remainder operations are actually 
 ; performed in the normal-order evaluation of (gcd 206 40)? In the applicative-order evaluation?
+
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+; Normal-order
+(gcd 206 40)
+(if (= 40 0)) ; ...
+(gcd 40 (remainder 206 40))
+(if (= 6 0)) ; ...
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+(if (= 4 0)) ; ...
+(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+(if (= 2 0)) ; ...
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+(if (= 0 0)) ; ...
+; => 2 
+
+; Application-order
+(gcd 206 40)
+(gcd 40 (remainder 206 40))
+(gcd 40 6)
+(if (= 6 0)) ; ...
+(gcd 6 (remainder 40 6))
+(gcd 6 4)
+(if (= 4 0)) ; ...
+(gcd 4 (remainder 6 4))
+(gcd 4 2)
+(if (= 2 0)) ; ...
+(gcd 2 (remainder 4 2))
+(gcd 2 0)
+(if (= 0 0)) 
+; => 2
